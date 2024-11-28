891c84b2e3df61bc3eb61693b95e1350d690e25a
Juliet Chuon | itsjuliet <j-chuon@hotmail.com>
2024-11-27 21:30:48 -0800

    update homepage title

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
 import './HomePage.scss'
 import ArrowIcon from '../../assets/icons/right-arrow.svg'
 
 function HomePage() {
+    useEffect(() => {
+        document.title = "PlatePal";
+      }, []);
+
   return (
     <div className='homepage'>
         <div className='homepage__container'>
